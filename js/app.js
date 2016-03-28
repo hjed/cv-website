31e0a8e536c6888fc1b84b20cfeda3cedd0e17aa
hjed <harry@dayfamilyweb.com>
2016-03-28 22:45:56 +1100

    Initial file creation and basic design

---
+++ js/app.js
@@ -0,0 +1,13 @@
+app = angular.module('cv', ['ngRoute']);
+
+app.controller('pageSummary', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
+    
+});
+
+app.controller('imageBlurb', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
+    
+});
+
+app.controller('normalBlurb', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
+    
+});
