3eb4049b50c25e8bffa34ae5f17bb37e5de52572
hjed <harry@dayfamilyweb.com>
2016-03-28 23:31:36 +1100

    Created nav, started working on titlebar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
-app = angular.module('cv', ['ngRoute']);
+var app = angular.module('cv', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+  
+  $routeProvider
+    .when('/', {
+      controller:'DefaultViewController',
+      templateUrl:'templates/normal.html',
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+    
+    $locationProvider.html5Mode(true);
+})
+
+app.controller('Main', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
+    
+}]);
+
+app.controller('DefaultViewController', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
+    
+}]);
 
 app.controller('pageSummary', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
     
-});
+}]);
 
 app.controller('imageBlurb', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
     
-});
+}]);
 
 app.controller('normalBlurb', ['$scope','$rootScope','$window', function($scope,$rootScope,$window) {
     
-});
+}]);
